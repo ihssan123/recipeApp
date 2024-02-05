05c579e62169ea72a23cab7249886550727ed86d
ihssan elfaquir <nasshi2001@gmail.com>
2024-02-05 17:54:47 +0100

    app recipe

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[]=[
+    new Recipe('A test recipe','This is simply a test','https://www.eatingwell.com/recipe/255186/chickpea-curry-chhole/')
+  ];
+  constructor(){
+
+  }
+  ngOnInit(): void {
+      
+  }
+
+}
